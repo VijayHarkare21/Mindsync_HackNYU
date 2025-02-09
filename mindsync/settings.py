13c0d9a279d642189a6a106c9c72eaca10a11316
Vijay Harkare <77393851+VijayHarkare21@users.noreply.github.com>
2025-02-09 12:44:54 -0500

    Update settings.py

--- mindsync/settings.py
+++ mindsync/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7(=ju1(g%818$9+1mbqgk2o#$ri$@*dbjq^x4%%(k__b5&47f-'
+SECRET_KEY = '<your_secret_key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
